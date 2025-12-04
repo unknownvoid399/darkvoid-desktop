a535f12a500b66e6bf0b75bf0eec66215fc78e1b
unknownvoid399 <christopher.carter26@cedarvilleschools.org>
2025-12-04 11:06:28 -0600

    Initialize Electron main process with window creation
    Set up the Electron main process to create a browser window with development and production configurations.

---
+++ electron-main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+const isDev = process.env.NODE_ENV !== 'production';
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1100,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  if (isDev) {
+    win.loadURL('http://localhost:5173');
+    win.webContents.openDevTools();
+  } else {
+    win.loadFile(path.join(__dirname, 'renderer', 'dist', 'index.html'));
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
